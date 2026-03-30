005c034436ca3e22ef9869adf0a780748a6d3ca8
Francisco Cardoso <francisco.cardoso.0@icloud.com>
2026-03-30 21:37:50 +0200

    fix

--- config/module.go
+++ config/module.go
@@ -5,11 +5,16 @@ import "time"
 type Module struct {
 	Name string
 	Path string
-	
+	RebootsAllowed int // amount of reboots allowed before shutdown is called
+
+	Init bool // ready to use
 	Times
-	CallingInterval time.Duration
+	CallingInterval time.Duration // how often the module should be called, if zero deactivated
+	
+	AmountOfReboots int
 }
 
 type Times struct {
-	TimeReady time.Time
+	TimeReady time.Time // time it took to be ready
+	TimeLastUpdate time.Time // time it was last updated
 }	
