107ab19741cf6978a1e1a50770b04002128b21d8
Francisco Cardoso <francisco.cardoso.0@icloud.com>
2026-03-31 10:36:23 +0200

    Module discovery

--- config/config.go
+++ config/config.go
@@ -1,11 +1,20 @@
 package config
 
-import "github.com/AgentNemo00/sca-instruments/api/router"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/AgentNemo00/sca-instruments/api/router"
+	"github.com/AgentNemo00/sca-instruments/errors"
+)
 
 type Config struct {
 	router.Config
-	Modules []Module
 	PubSubUrl string
+
+	Modules []Module
+
+	mu sync.RWMutex
 	// TODO: output to draw to
 }
 
@@ -14,7 +23,7 @@ func (c *Config) Default() {
 		c.Port = 10001
 	}
 	if c.Name == "" {
-		c.Name = "kigo"
+		c.Name = "KigoMainService"
 	}
 	if c.Version == "" {
 		c.Version = "1.0.0"
@@ -27,11 +36,27 @@ func (c *Config) Default() {
 }
 
 func (c *Config) GetModule(name string) (*Module, int) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	for index, module := range c.Modules {
 		if module.Name == name {
 			return &module, index
 		}
 	}
 	return nil, -1
+}
+
+func (c *Config) CreateModule(name string) (*Module, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	module, _ := c.GetModule(name)
+	if module != nil {
+		return nil, errors.New(fmt.Errorf("A module with this name already exits"))
+	}
+	newModule := Module{
+		Name: name
+	}
+	c.Modules = append(c.Modules, newModule)
+	return &newModule
 }
 
